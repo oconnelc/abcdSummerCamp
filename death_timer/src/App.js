ea24585d675fda10382534732c7c0fb78a62245e
Christopher O'Connell <coconnell84@gmail.com>
2020-07-05 15:09:52 -0400

    Got the timing logic working

--- death_timer/src/App.js
+++ death_timer/src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [seconds, setSeconds] = useState(10);
+  const [isActive, setIsActive] = useState(true);
+
+  function reset() {
+    setSeconds(10);
+    setIsActive(true);
+  }
+
+  useEffect(() =>{
+    let interval = null;
+
+    if(isActive) {
+      interval = setInterval(() => {
+        if(seconds == 0) {
+          setIsActive(false);
+        } else {
+          setSeconds(seconds => seconds -1);
+        }
+        
+      },1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+
   return (
     <div className="App">
       <div className="TimerText">
-        Timer Text
+        {seconds}
       </div>
     </div>
   );
